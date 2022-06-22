ec5a0fda955a9190f7964fb1157037a68945e457
AchmadWahyu <achwahyu.w@gmail.com>
2022-06-22 15:52:02 +0700

    feat: add fetching indicator

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="navbar bg-base-100 text-center">
       <div className="flex-1">
@@ -10,7 +13,20 @@ export default function Header() {
           </a>
         </Link>
       </div>
-      <div className="flex-none">
+      {router.pathname === '/about-us' ? (
+        <div className="flex-1 justify-center">
+          <h2 className="text-xl text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-rose-400">
+            SSG
+          </h2>
+        </div>
+      ) : (
+        <div className="flex-1 justify-center">
+          <h2 className="text-xl text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-rose-400">
+            SSR
+          </h2>
+        </div>
+      )}
+      <div className="flex-1">
         <Link href="/about-us">
           <a className="btn btn-ghost normal-case text-xl text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-rose-400">
             About Us
